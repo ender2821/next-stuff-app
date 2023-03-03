b2aab9f18e3a13d8c489c7472929f7f71190fd5e
Joshua Jensen <ender2821@yahoo.com>
2023-03-03 15:22:12 -0700

    update primary and secondary screens with new design

--- app/(user)/vehicles/PageView.tsx
+++ app/(user)/vehicles/PageView.tsx
@@ -1,19 +1,20 @@
 "use client";
 
 import React, { useContext, useEffect, useState } from "react";
-import { useRouter, usePathname } from 'next/navigation';
+import { useRouter, usePathname } from "next/navigation";
 import urlFor from "../../../lib/urlFor";
 import Image from "next/image";
 import Link from "next/link";
 import appContext from "../../../lib/appContext";
 
 import DeleteIcon from "../../../assets/delete-icon.svg";
-import SubmitIcon from "../../../assets/submit-icon-light.svg"
-import AddIcon from '../../../assets/add-icon.svg';
-import VehicleIcon from '../../../assets/vehicle-icon.svg';
-import ToolsIcon from '../../..//assets/specs-icon.svg';
-import GearIcon from '../../../assets/gear-icon.svg';
-import LifeIcon from '../../../assets/life-icon.svg';
+import SubmitIcon from "../../../assets/submit-icon-light.svg";
+import AddIcon from "../../../assets/add-icon.svg";
+import VehicleIcon from "../../../assets/vehicle-icon.svg";
+import ToolsIcon from "../../..//assets/specs-icon.svg";
+import GearIcon from "../../../assets/gear-icon.svg";
+import LifeIcon from "../../../assets/life-icon.svg";
+import AddSomeImage from "../../../assets/add-some-schit-image-light.svg";
 
 import styles from "./listPage.module.scss";
 
@@ -31,8 +32,8 @@ type PageProps = {
 export default function PageView(props: PageProps) {
   const { data } = props;
   const { setSecondaryLayout, setTitleText } = useContext(appContext);
-  const [ newItem, setNewItem ] = useState(false)
-  const [ newItemName, setNewItemName ] = useState('');
+  const [newItem, setNewItem] = useState(false);
+  const [newItemName, setNewItemName] = useState("");
 
   const router = useRouter();
   const path = usePathname();
@@ -44,54 +45,64 @@ export default function PageView(props: PageProps) {
 
   const iconRender = () => {
     const icon = path;
-    switch(icon) {
-      case '/vehicles':
-        return <VehicleIcon />
-      case '/tools':
-        return <ToolsIcon />
-      case '/gear':
-        return <GearIcon />
-      case '/life':
-        return <LifeIcon />
-      default: 
-        return
+    switch (icon) {
+      case "/vehicles":
+        return <VehicleIcon />;
+      case "/tools":
+        return <ToolsIcon />;
+      case "/gear":
+        return <GearIcon />;
+      case "/life":
+        return <LifeIcon />;
+      default:
+        return;
     }
-  }
-  
+  };
+
   const onAddSubmit = async () => {
-    if ( newItemName.length > 0 ) {
-      const id = 'vehicle';
+    if (newItemName.length > 0) {
+      const id = "vehicle";
 
       const slugify = (str: string) => {
         return str
-        .toLowerCase()
-        .trim()
-        .replace(/[^\w\s-]/g, '')
-        .replace(/[\s_-]+/g, '-')
-        .replace(/^-+|-+$/g, '');
-      }
+          .toLowerCase()
+          .trim()
+          .replace(/[^\w\s-]/g, "")
+          .replace(/[\s_-]+/g, "-")
+          .replace(/^-+|-+$/g, "");
+      };
 
-      await fetch('/api/_addCategory', {
-        method: 'post',
-        body: JSON.stringify({id: id, name: newItemName, slug: slugify(newItemName)}),
-      }).then(() => {
-        setNewItem(false);
-        setNewItemName('');
-        router.replace(path as string);
-      }).catch((error) => console.log(error));
-    } 
-  }
+      await fetch("/api/_addCategory", {
+        method: "post",
+        body: JSON.stringify({
+          id: id,
+          name: newItemName,
+          slug: slugify(newItemName),
+        }),
+      })
+        .then(() => {
+          setNewItem(false);
+          setNewItemName("");
+          router.replace(path as string);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
 
   const onDelete = async (id: string) => {
-    await fetch('/api/_deleteCategory', {
-      method: 'post',
-      body: JSON.stringify({id: id}),
-    }).then(() => {
-      router.replace(path as string);
-    }).catch((error) => console.log(error));
-  }
+    await fetch("/api/_deleteCategory", {
+      method: "post",
+      body: JSON.stringify({ id: id }),
+    })
+      .then(() => {
+        router.replace(path as string);
+      })
+      .catch((error) => console.log(error));
+  };
 
-  const handleNewItemNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleNewItemNameChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const value = event.target.value;
     setNewItemName(value);
   };
@@ -99,18 +110,20 @@ export default function PageView(props: PageProps) {
   return (
     <>
       <ul className={styles.itemList}>
-        {data.map((item: ListPage, i: number) => {
+        {data?.map((item: ListPage, i: number) => {
           return (
             <li key={i} className={styles.listItem}>
-              <Link
-                href={`/vehicles/${item?.slug?.current}`}
-              >
+              <Link href={`/vehicles/${item?.slug?.current}`}>
                 <div className={styles.imageContain}>
-                  {item?.image && <Image
-                    src={urlFor(item?.image).url()}
-                    alt={item?.name}
-                    fill
-                  />}
+                  {item?.image ? (
+                    <Image
+                      src={urlFor(item?.image).url()}
+                      alt={item?.name}
+                      fill
+                    />
+                  ) : (
+                    <div className={styles.iconContain}>{iconRender()}</div>
+                  )}
                 </div>
                 <div className={styles.content}>
                   <p>{item?.name}</p>
@@ -124,19 +137,28 @@ export default function PageView(props: PageProps) {
         })}
         {newItem && (
           <li className={styles.listItem}>
-            <div >
-              <div className={styles.imageContain}>
-                {iconRender()}
-              </div>
+            <div className={styles.newItem}>
+              <div className={styles.iconContain}>{iconRender()}</div>
 
               <div className={styles.content}>
-                <input onChange={handleNewItemNameChange} value={newItemName}/>
+                <div className={styles.inputContain}>
+                  <input
+                    onChange={handleNewItemNameChange}
+                    value={newItemName}
+                  />
+                </div>
                 <button onClick={onAddSubmit}>
                   <SubmitIcon />
                 </button>
               </div>
             </div>
           </li>
+        )}
+        {data?.length === 0 && !newItem && (
+          <div className={styles.addSomeContain}>
+            <p>Add some schit</p>
+            <AddSomeImage />
+          </div>
         )}
       </ul>
       <button onClick={() => setNewItem(true)} className={styles.addButton}>
--- app/(user)/vehicles/[slug]/PageView.tsx
+++ app/(user)/vehicles/[slug]/PageView.tsx
@@ -1,84 +1,84 @@
 "use client";
 
 import React, { useContext, useEffect } from "react";
-import styles from './vehicleHome.module.scss';
-import Image  from 'next/image';
-import urlFor from '../../../../lib/urlFor'
-import appContext from '../../../../lib/appContext'
+import styles from "./vehicleHome.module.scss";
+import Image from "next/image";
+import urlFor from "../../../../lib/urlFor";
+import appContext from "../../../../lib/appContext";
 import Link from "next/link";
 
-import InfoIcon from '../../../../assets/info-icon.svg';
-import ToDoIcon from '../../../../assets/todo-icon.svg';
-import ToBuyIcon from '../../../../assets/tobuy-icon.svg';
-import OwnedIcon from '../../../../assets/owned-icon.svg';
-import SpecsIcon from '../../../../assets/specs-icon.svg';
+import InfoIcon from "../../../../assets/info-icon.svg";
+import ToDoIcon from "../../../../assets/todo-icon.svg";
+import ToBuyIcon from "../../../../assets/tobuy-icon.svg";
+import OwnedIcon from "../../../../assets/owned-icon.svg";
+import SpecsIcon from "../../../../assets/specs-icon.svg";
+import VehicleImage from "../../../../assets/vehicle-image.svg";
 
 type PageProps = {
-  data: Vehicle,
-  slug: string,
-}
+  data: Vehicle;
+  slug: string;
+};
 
-export default function PageView(props:PageProps) {
-  const { data, slug } = props
+export default function PageView(props: PageProps) {
+  const { data, slug } = props;
   const { setSecondaryLayout, setTitleText } = useContext(appContext);
 
   useEffect(() => {
     setSecondaryLayout(true);
     setTitleText(data?.name);
-  }, [data?.name, setSecondaryLayout, setTitleText])
+  }, [data?.name, setSecondaryLayout, setTitleText]);
 
   return (
     <>
       <div className={styles.imageContain}>
-        {data?.image && <Image
-          src={urlFor(data?.image).url()}
-          alt={data?.name}
-          fill
-        />
-        }
+        {data?.image ? (
+          <Image src={urlFor(data?.image).url()} alt={data?.name} fill />
+        ) : (
+          <VehicleImage />
+        )}
       </div>
       <ul className={styles.linkList}>
         <li>
-          <Link
-            href={`/vehicles/${slug}/info`}
-          >
-            <span className={styles.iconContain}><InfoIcon /></span>
+          <Link href={`/vehicles/${slug}/info`}>
+            <span className={styles.iconContain}>
+              <InfoIcon />
+            </span>
             <span className={styles.text}>{data?.name} Info</span>
           </Link>
         </li>
         <li>
-          <Link
-            href={`/vehicles/${slug}/todo`}
-          >
-            <span className={styles.iconContain}><ToDoIcon /></span>
+          <Link href={`/vehicles/${slug}/todo`}>
+            <span className={styles.iconContain}>
+              <ToDoIcon />
+            </span>
             <span className={styles.text}>Schit to do</span>
           </Link>
         </li>
         <li>
-          <Link
-            href={`/vehicles/${slug}/tobuy`}
-          >
-            <span className={styles.iconContain}><ToBuyIcon /></span>
+          <Link href={`/vehicles/${slug}/tobuy`}>
+            <span className={styles.iconContain}>
+              <ToBuyIcon />
+            </span>
             <span className={styles.text}>Schit to buy</span>
           </Link>
         </li>
         <li>
-          <Link
-            href={`/vehicles/${slug}/owned`}
-          >
-            <span className={styles.iconContain}><OwnedIcon /></span>
+          <Link href={`/vehicles/${slug}/owned`}>
+            <span className={styles.iconContain}>
+              <OwnedIcon />
+            </span>
             <span className={styles.text}>Schit owned</span>
           </Link>
         </li>
         <li>
-          <Link
-            href={`/vehicles/${slug}/specs`}
-          >
-            <span className={styles.iconContain}><SpecsIcon /></span>
+          <Link href={`/vehicles/${slug}/specs`}>
+            <span className={styles.iconContain}>
+              <SpecsIcon />
+            </span>
             <span className={styles.text}>Schit specs</span>
           </Link>
         </li>
       </ul>
     </>
-  )
+  );
 }
